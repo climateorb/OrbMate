96006f3a0fb2bdea88405ddaf06edb58667053c1
shachindra <shachindra.spidey@gmail.com>
2019-09-12 18:37:10 +0800

    Device ID

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,7 @@
 //index.js
-//获取应用实例
 const app = getApp()
 
 Page({
-  //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
@@ -16,8 +14,6 @@ Page({
         hasUserInfo: true
       })
     } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
         this.setData({
           userInfo: res.userInfo,
@@ -25,7 +21,6 @@ Page({
         })
       }
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
         success: res => {
           app.globalData.userInfo = res.userInfo
@@ -44,5 +39,12 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+  orb: {
+    deviceId: "123456789"
+  },
+  showData: function () {
+    var devId = this.orb.deviceId;
+    console.log(devId);
   }
 })
